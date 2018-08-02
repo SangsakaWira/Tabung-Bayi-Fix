14844617e5e97d4d410f93e1b8487cd6292bde9a
Sangsaka Wira <sangsakawira@gmail.com>
2018-08-02 23:21:19 +0700

    happened

--- app.js
+++ app.js
@@ -3,3 +3,27 @@ const app = express();
 const http = require('http');
 const hbs = require('hbs');
 const socketIO = require('socket.io');
+
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.get("/:value",function(req,res){
+    myValue = req.params.value;
+    res.render("data.hbs",{
+        data:myValue
+    });
+})
+
+io.on('connection',function(socket){
+    console.log("A user connected");
+    socket.on('data', function(data){
+        console.log("Suhu: " + data);
+      });
+})
+
+server.listen(3000,function(){
+    console.log("Server is running");
+})
